7604c6859709927dfee9634367b3a312b8971533
kevinmr59 <kevinmura59610@gmail.com>
2021-11-10 14:14:23 +0100

    fini

---
+++ add.js
@@ -0,0 +1,10 @@
+let result = 5+6;
+console.log(result);
+let addition = result +2;
+console.log(addition)
+let soustraction = result -2;
+console.log(soustraction);
+alert(result*addition)
+alert(result**soustraction)
+let modulo=5%2;
+console.log(result)
